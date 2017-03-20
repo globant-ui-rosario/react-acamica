2b84f03b98534d08358c781de66c8cc4f1e61d07
Nahuel Jesús Sacchetti <nahuel.sacchetti@wnco.com>
2017-03-20 14:02:03 -0300

    Added 1.10

---
+++ 1.10/1.10.js
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+import React from 'react';
+
+export default class Component extends React.Component {
+
+    constructor () {
+        super();
+
+        this.state = {
+            selected: false
+        };
+    }
+
+    render () {
+        return (
+            <p className={this.getClass()}>
+                This is a text block
+            </p>
+        );
+    }
+
+    getClass () {
+        return classNames({
+            block: true,
+            'text-block': (this.props.textBlock),
+            'text-block--highlighted': (this.state.selected)
+        });
+    }
+}
