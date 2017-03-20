252f97e663d1e44965554de28be26ac611d6bb89
Ezequiel Alvarez <ezequiel.alvarez@globant.com>
2017-03-20 16:48:42 -0300

    add examples 1.5

---
+++ 1.5/1.5.1.js
@@ -0,0 +1,16 @@
+class TaskItem extends React.Component {
+    render () {
+        return <li>{this.props.name}</li>;
+    }
+}
+
+class TasksList extends React.Component {
+    render () {
+        return (
+            <ul>
+                <TaskItem name="Do homework" />
+                <TaskItem name="Work" />
+            </ul>
+        );
+    }
+}
