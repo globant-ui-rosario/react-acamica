c18590eb7d56a5ae823c1a13de3f133163b8b97c
Andres Troiano <troianoandres@gmail.com>
2017-03-20 15:51:20 -0300

    [1.3]
    Updated example for [1.3]

--- 1.3/1.3.1.js
+++ 1.3/1.3.1.js
@@ -1,9 +1,5 @@
-function MyComponent (props) {
-    return React.createElement('h1', null, 'Hello, ', props.name);
-}
-
 class MyComponent extends React.Component {
     render () {
-        return React.createElement('h1', null, 'Hello, ', this.props.name);
+        return React.createElement('h1', null, 'Hello world');
     }
 }
