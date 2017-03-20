6c54616a3b64c751b791325b62f73253029e044d
Ezequiel Alvarez <ezequiel.alvarez@globant.com>
2017-03-20 17:03:29 -0300

    added 1.5.3 example

---
+++ 1.5/1.5.3.js
@@ -0,0 +1,20 @@
+class TaskItem extends React.Component {
+    render () {
+        return <li>{this.props.children}</li>;
+    }
+}
+
+class TasksList extends React.Component {
+    render () {
+        return (
+            <ul>
+                <TaskItem>
+                    Do homework
+                </TaskItem>
+                <TaskItem>
+                    Run
+                </TaskItem>
+            </ul>
+        );
+    }
+}
