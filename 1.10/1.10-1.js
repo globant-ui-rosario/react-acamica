7a0fefd6204a2f75a92cf0b2a1283d7b1e720199
Nahuel Jesús Sacchetti <elnawe@users.noreply.github.com>
2017-05-21 16:45:13 -0300

    Create 1.10-1.js

---
+++ 1.10/1.10-1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Component extends React.Component {
+    
+    render () {
+        return (
+            <p className="test-class">
+                Using className
+            </p>
+        );
+    }
+}
