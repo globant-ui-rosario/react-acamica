1a56442dff7ab9c5345aba681c626df78f9088c9
Nahuel Jesús Sacchetti <elnawe@users.noreply.github.com>
2017-05-21 18:01:33 -0300

    Update 3.3-1.js

--- 3.3/3.3-1.js
+++ 3.3/3.3-1.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router'
 
-}{
+export default class Component extends React.Component {
+    
+    render () {
+        return (
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/users">Address</Link>
+                <Link to="/about">About</Link>
+            </div>
+        );
+    }
+}
